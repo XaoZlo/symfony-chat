39fb09a845eb2a2add5f17765877d996bb056aa9
timur <timur962@gmail.com>
2019-04-16 20:13:00 +0500

    Docker changes

--- docker/nginx/nginx_start.sh
+++ docker/nginx/nginx_start.sh
@@ -1,5 +1,4 @@
 !/bin/bash
-rm /etc/nginx/sites-enabled/default
 echo "Устанавливаем ссылки конфига"
 ln -sf /etc/nginx/sites-available/symfony.conf /etc/nginx/sites-enabled/symfony
 echo "Запускаем сервак"
